58c1e23b9280b361470c2158c8e1ee0d36b834b7
Tim Schindler <tim.schindler@gmail.com>
2021-12-15 10:42:00 +0100

    working unit tests

--- Invoke-PASAccountPasswordChange.Tests.ps1
+++ Invoke-PASAccountPasswordChange.Tests.ps1
@@ -3,16 +3,35 @@
 
     $AAMClientPath = 'C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe'
     $AppID = 'windowsScript'
-    $CredentialClixmlPath = TestDrive:\Credential.ps1.credential
-    "Example" | Export-Clixml -Path $CredentialClixmlPath
+    $CredentialClixmlPath = 'TestDrive:\Credential.ps1.credential'
+
+    New-Object System.Management.Automation.PSCredential("DemoUser", ('DemoPass' | ConvertTo-SecureString -AsPlainText -Force)) | Export-Clixml -Path $CredentialClixmlPath
 }
 
 Describe "Invoke-PASAccountPasswordChange" {
     It "invokes an immediate password change" {
-        Mock Invoke-PASCPMOperation -MockWith {} -ModuleName psPAS
+        Mock Test-Path -MockWith { return $True } -ModuleName CredentialRetriever
+        Mock Get-AIMCredential -MockWith {
+            $OutputObject = [PSCustomObject]@{
+                Password = 'DemoPass'
+                UserName = 'DemoUser'
+                Address  = 'DOMAIN.COM'
+            }
+            $OutputObject | Add-Member -MemberType ScriptMethod -Name ToSecureString -Value {
+                $this.Password | ConvertTo-SecureString -AsPlainText -Force
+            } -Force
 
-        Invoke-PASAccountPasswordChange -AccountId 1_1 -AAMClientPath $AAMClientPath -AppID $AppID
+            $OutputObject | Add-Member -MemberType ScriptMethod -Name ToCredential -Value {
+                New-Object System.Management.Automation.PSCredential($this.UserName, $this.ToSecureString())
+            } -Force
+            return $OutputObject
+        }
+        Mock New-PASSession -MockWith {}
+        Mock Close-PASSession -MockWith {}
+        Mock Invoke-PASCPMOperation -MockWith {}
 
-        Should -Invoke -CommandName Invoke-PASCPMOperation -ParameterFilter {$ChangeImmediate -eq $true}
+        Invoke-PASAccountPasswordChange -AccountId 1_1 -AAMClientPath $AAMClientPath -AppID $AppID -PVWAAddress https://comp01 -NewPasswordClixmlPath $CredentialClixmlPath
+
+        Should -Invoke -CommandName Invoke-PASCPMOperation -ParameterFilter { $ChangeImmediately -eq $true -and $ChangeTask}
     }
 }
--- Invoke-PASAccountPasswordChange.ps1
+++ Invoke-PASAccountPasswordChange.ps1
@@ -16,8 +16,10 @@ function Invoke-PASAccountPasswordChange {
     $ApiCredential = Get-AIMCredential -AppID $AppID -Safe $Safe -UserName $UserName -Address $Address
     $ApiCredential = $ApiCredential.ToCredential()
 
+
     New-PASSession -BaseURI $PVWAAddress -type CyberArk -Credential $ApiCredential -concurrentSession $true
     $NewPassword = Import-Clixml $NewPasswordClixmlPath
-    Invoke-PASCPMOperation -AccountID $AccountId -ChangeImmediately $true -NewCredentials $NewPassword
+    Invoke-PASCPMOperation -AccountID $AccountId -ChangeTask -ChangeImmediately $true -NewCredentials $NewPassword.Password
     Close-PASSession
+
 }
