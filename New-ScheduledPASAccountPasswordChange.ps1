d770cea6bb4a2d75a7771b3daf8bdeee98ac48b1
Tim Schindler <tim.schindler@gmail.com>
2021-12-15 11:21:23 +0100

    actually creates a scheduled task now

--- New-ScheduledPASAccountPasswordChange.ps1
+++ New-ScheduledPASAccountPasswordChange.ps1
@@ -33,14 +33,17 @@ function New-ScheduledPASAccountPasswordChange {
         Copy-Item -Path "$PSScriptRoot/Invoke-PASAccountPasswordChange.ps1" -Destination $WorkingDirectory
         # Export the future password as an encrypted object to the working directory
         # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/export-clixml?view=powershell-5.1#example-3--encrypt-an-exported-credential-object
-        $CredentialFilePath = "$WorkingDirectory/ChangeTask.ps1.pscredential"
-        $Password | Export-Clixml $CredentialFilePath
+        $CredentialFilePath = "$WorkingDirectory/Invoke-PASAccountPasswordChange.ps1.pscredential"
+        # When wanting to save soemthing encrypted, Export-Clixml seems to expect that the input
+        # PSCredential object so we need to convert the password as secure string to a PSCredential
+        $PasswordCredentialObject = New-Object System.Management.Automation.PSCredential("dummydata", $Password)
+        $PasswordCredentialObject | Export-Clixml $CredentialFilePath
 
         # Create scheduled task
-        $ChangeTaskScriptBlock = "PowerShell.exe -C ./$WorkingDirectory/Invoke-PASAccountPasswordChange.ps1 -AccountId $AccountId -NewPasswordClixmlPath $CredentialFilePath -AppId windowsScript C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe -UserName serviceAccount01 -Address iosharp.lab -Safe Windows -PVWAAddress https://comp01/PasswordVault"
-        $ScheduledTaskAction = New-ScheduledTaskAction -Execute $ChangeTaskScriptBlock
-        $ScheduledTaskTrigger = New-ScheduledTaskTrigger -At $ChangeTime
-        $ScheduledTaskPrincipal = New-ScheduledTaskPrincipal -UserId [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+        $ChangeTaskScriptBlock = "$WorkingDirectory\Invoke-PASAccountPasswordChange.ps1 -AccountId $AccountId -NewPasswordClixmlPath $CredentialFilePath -AppId windowsScript `"C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe`" -UserName serviceAccount01 -Address iosharp.lab -Safe Windows -PVWAAddress https://comp01/PasswordVault"
+        $ScheduledTaskAction = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-NoProfile -WindowStyle Hidden -Command $ChangeTaskScriptBlock"
+        $ScheduledTaskTrigger = New-ScheduledTaskTrigger -At $ChangeTime -Once
+        $ScheduledTaskPrincipal = New-ScheduledTaskPrincipal -UserId ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)
         $ScheduledTask = New-ScheduledTask -Action $ScheduledTaskAction -Trigger $ScheduledTaskTrigger -Principal $ScheduledTaskPrincipal
 
         Register-ScheduledTask "Password Change for $AccountId" -InputObject $ScheduledTask
