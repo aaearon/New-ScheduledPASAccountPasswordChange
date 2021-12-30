7e77a7858f7cdb50a7b320d6e7a87cdd346035b6
Tim Schindler <tim.schindler@gmail.com>
2021-12-30 18:22:30 +0100

    better documentation

--- New-ScheduledPASAccountPasswordChange.ps1
+++ New-ScheduledPASAccountPasswordChange.ps1
@@ -1,42 +1,61 @@
 ﻿#Requires -Module psPAS
 function New-ScheduledPASAccountPasswordChange {
+    <#
+    .SYNOPSIS
+        Creates a Scheduled Task that will execute a script at a specific time to change an account's password.
+    .DESCRIPTION
+        Using Windows Scheduled Tasks, this PowerShell script will create a Scheduled Task that runs at a specific time and will execute a script to change the provided account's password. The password is stored as
+        an exported credential object.
+    .EXAMPLE
+        PS C:\> New-ScheduledPASAccountPasswordChange -AccountId 12_7 -AppID scheduledPasswordChangeScript -ChangeTime ((Get-Date) AddDays(5)) -PvwaAddress https://comp01 -Password ("VeryComplex!23" | ConvertTo-SecureString -AsPlainText -Force) -Safe CyberArk -UserName serviceAccount01 -Address 192.168.0.50 -CentralCredentialProviderURL https://comp01
+        For the account with the id of 12_7, a Scheduled Task is created to execute a password change 5 days from now to the value "VeryComplex!23". The Central Credential Provider is leveraged to pull the password for the service account that will authenticate to the CyberArk PVWA (https://comp01) and perform the change.
+    #>
     [CmdletBinding()]
     param (
+        # The id of the account (i.e 12_6)
         [Parameter(ParameterSetName = 'CredentialProvider', Mandatory = $true)]
         [Parameter(ParameterSetName = 'CentralCredentialProvider', Mandatory = $true)]
         [string]$AccountId,
 
+        # The future password as a secure string.
         [Parameter(ParameterSetName = 'CredentialProvider', Mandatory = $true)]
         [Parameter(ParameterSetName = 'CentralCredentialProvider', Mandatory = $true)]
         [securestring]$Password,
 
-        # Parameter help description
+        # The date and time the password should be changed. The Scheduled Task will run at this time.
         [Parameter(ParameterSetName = 'CredentialProvider', Mandatory = $true)]
         [Parameter(ParameterSetName = 'CentralCredentialProvider', Mandatory = $true)]
         [datetime]$ChangeTime,
 
+        # The address of the PVWA for the environment the account resides in.
         [Parameter(ParameterSetName = 'CredentialProvider', Mandatory = $true)]
         [Parameter(ParameterSetName = 'CentralCredentialProvider', Mandatory = $true)]
         [string]$PvwaAddress,
 
+        # The URL of the Central Credential Provider instance. The password for the service account that will be used to execute the password change will be retrieved from here.
         [Parameter(ParameterSetName = 'CentralCredentialProvider', Mandatory = $true)]
         [string]$CentralCredentialProviderURL,
 
+        # The full path of where the locally installed Credential Provider agent is installed. The password for the service account that will be used to execute the password change will be retrieved using the CP installed here.
         [Parameter(ParameterSetName = 'CredentialProvider', Mandatory = $true)]
         [string]$CredentialProviderPath,
 
+        # The AppID to authenticate to the Credential Provider or Central Credential Provider with. The AppID should have the needed authorizations to get the password for the serivce account performing the change.
         [Parameter(ParameterSetName = 'CredentialProvider', Mandatory = $true)]
         [Parameter(ParameterSetName = 'CentralCredentialProvider', Mandatory = $true)]
         [string]$AppID,
 
+        # The safe where the service account that will be used to perform the password change is stored.
         [Parameter(ParameterSetName = 'CredentialProvider', Mandatory = $true)]
         [Parameter(ParameterSetName = 'CentralCredentialProvider', Mandatory = $true)]
         [string]$Safe,
 
+        # The username of the service account that will be used to perform the password change.
         [Parameter(ParameterSetName = 'CredentialProvider', Mandatory = $true)]
         [Parameter(ParameterSetName = 'CentralCredentialProvider', Mandatory = $true)]
         [string]$UserName,
 
+        # The address of the service account that will be used to perform the password change.
         [Parameter(ParameterSetName = 'CredentialProvider', Mandatory = $true)]
         [Parameter(ParameterSetName = 'CentralCredentialProvider', Mandatory = $true)]
         [string]$Address
@@ -57,20 +76,23 @@ function New-ScheduledPASAccountPasswordChange {
         # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/export-clixml?view=powershell-5.1#example-3--encrypt-an-exported-credential-object
         $CredentialFilePath = "$WorkingDirectory/Invoke-PASAccountPasswordChange.ps1.pscredential"
         # When wanting to save soemthing encrypted, Export-Clixml seems to expect that the input
-        # PSCredential object so we need to convert the password as secure string to a PSCredential
+        # PSCredential object so we need to convert the password from the provided secure string to a PSCredential
         $PasswordCredentialObject = New-Object System.Management.Automation.PSCredential("dummydata", $Password)
         $PasswordCredentialObject | Export-Clixml $CredentialFilePath
 
-        # Create scheduled task
+        ## Create scheduled task
+        # The script that will perform the password change is called.
         switch ($PSCmdlet.ParameterSetName) {
             'CredentialProvider' {
-                $ChangeTaskScriptBlock = "{Import-Module $WorkingDirectory\Invoke-PASAccountPasswordChange.ps1; Invoke-PASAccountPasswordChange -AccountId $AccountId -NewCredentialClixmlPath $CredentialFilePath -PVWAAddress $PvwaAddress -AppID $AppID -UserName $UserName -Address $Address -Safe $Safe -CredentialProviderPath $CredentialProviderPath}"
+                $Provider = "-CredentialProviderPath $CredentialProviderPath}"
             }
 
             'CentralCredentialProvider' {
-                $ChangeTaskScriptBlock = "{Import-Module $WorkingDirectory\Invoke-PASAccountPasswordChange.ps1; Invoke-PASAccountPasswordChange -AccountId $AccountId -NewCredentialClixmlPath $CredentialFilePath -PVWAAddress $PvwaAddress -AppID $AppID -UserName $UserName -Address $Address -Safe $Safe -CentralCredentialProviderURL $CentralCredentialProviderURL}"
+                $Provider = "-CentralCredentialProviderURL $CentralCredentialProviderURL}"
             }
         }
+        $ChangeTaskScriptBlock = "{Import-Module $WorkingDirectory\Invoke-PASAccountPasswordChange.ps1; Invoke-PASAccountPasswordChange -AccountId $AccountId -NewCredentialClixmlPath $CredentialFilePath -PVWAAddress $PvwaAddress -AppID $AppID -UserName $UserName -Address $Address -Safe $Safe $Provider"
+
         $ScheduledTaskAction = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-NoProfile -WindowStyle Hidden -Command % `"$ChangeTaskScriptBlock`""
         $ScheduledTaskTrigger = New-ScheduledTaskTrigger -At $ChangeTime -Once
         # The scheduled task needs to run under the same user that creates the Export-Clixml as Windows
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ On the machine you are creating the Scheduled Task on:
 
 * [psPAS](https://github.com/pspete/psPAS)
 * [CredentialRetriever](https://github.com/pspete/CredentialRetriever)
-* Administrator rights
+* Administrator rights on the machine you are creataing the Scheduled Task on.
 
 ### Creating a Scheduled Task
 
@@ -18,5 +18,6 @@ On the machine you are creating the Scheduled Task on:
 1. Open an elevated PowerShell prompt and run `Import-Module New-ScheduledPASAccountPasswordChange`.
 1. Run `New-ScheduledPASAccountPasswordChange`
 
-       New-ScheduledPASAccountPasswordChange -AccountId 12_7 -AppID scheduledPasswordChangeScript -ChangeTime ((Get-Date) AddSeconds(5)) -PvwaAddress https://comp01 -Password ("VeryComplex!23" | ConvertTo-SecureString -AsPlainText -Force) -Safe CyberArk -UserName serviceAccount01 -Address 192.168.0.50 -CentralCredentialProviderURL https://comp01
+>   New-ScheduledPASAccountPasswordChange -AccountId 12_7 -AppID scheduledPasswordChangeScript -ChangeTime ((Get-Date) AddSeconds(5)) -PvwaAddress https://comp01 -Password ("VeryComplex!23" | ConvertTo-SecureString -AsPlainText -Force) -Safe CyberArk -UserName serviceAccount01 -Address 192.168.0.50 -CentralCredentialProviderURL https://comp01
+
 1. Double check in Task Scheduler that your new Task has been created.
